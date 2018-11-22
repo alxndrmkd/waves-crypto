68ffecf875d220a548539b72f20444d4e9ab9cb0
Alexandr Makoed <me@dsfn.gq>
2018-11-22 13:25:53 +0300

    Add Eq and Ord instances

--- src/Waves/Crypto/Types.hs
+++ src/Waves/Crypto/Types.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE StandaloneDeriving   #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 
 module Waves.Crypto.Types where
@@ -13,6 +14,15 @@ signature = C.signature
 
 signatureBytes = C.signatureBytes
 
+instance Show Signature where
+  show = show . encode58 . signatureBytes
+
+instance Eq Signature where
+  (==) s1 s2 = signatureBytes s1 == signatureBytes s2
+
+instance Ord Signature where
+  compare s1 s2 = signatureBytes s1 `compare` signatureBytes s2
+
 -------------------------------------------------
 
 type PublicKey = C.PublicKey
@@ -21,21 +31,34 @@ publicKey = C.publicKey
 
 publicKeyBytes = C.publicKeyBytes
 
+instance Show PublicKey where
+  show = show . encode58 . publicKeyBytes
+
+instance Eq PublicKey where
+  (==) pk1 pk2 = publicKeyBytes pk1 == publicKeyBytes pk2
+
+instance Ord PublicKey where
+  compare pk1 pk2 = publicKeyBytes pk1 `compare` publicKeyBytes pk2
+
+-------------------------------------------------
+
 type PrivateKey = C.PrivateKey
 
 privateKey = C.privateKey
 
 privateKeyBytes = C.privateKeyBytes
+
+instance Show PrivateKey where
+  show = show . encode58 . privateKeyBytes
+
+instance Eq PrivateKey where
+  (==) pk1 pk2 = privateKeyBytes pk1 == privateKeyBytes pk2
+
+instance Ord PrivateKey where
+  compare pk1 pk2 = privateKeyBytes pk1 `compare` privateKeyBytes pk2
+
 -------------------------------------------------
 
 type KeyPair = C.KeyPair
 
 -------------------------------------------------
-instance Show Signature where
-  show = show . encode58 . signatureBytes
-
-instance Show PublicKey where
-  show = show . encode58 . publicKeyBytes
-
-instance Show PrivateKey where
-  show = show . encode58 . privateKeyBytes
